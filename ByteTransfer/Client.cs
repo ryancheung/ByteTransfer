3146100d29a18fd40b95d64c2db4d1f49b723264
ryancheung <ryancheung.go@gmail.com>
2020-07-24 21:35:58 +0800

    Setup a timer to update client and socket

--- ByteTransfer/Client.cs
+++ ByteTransfer/Client.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Net.Sockets;
 
 namespace ByteTransfer
@@ -16,8 +17,12 @@ namespace ByteTransfer
         private T _socket;
         public T Socket { get { return _socket; } }
 
-        public bool Started { get; private set; }
-        public bool Stopped { get; private set; }
+        private bool _started;
+        public bool Started { get { return _started; } }
+        private bool _stopped;
+        public bool Stopped { get { return _stopped; } }
+
+        private Timer _updateTimer;
 
         public Client(string host, int port)
         {
@@ -27,12 +32,27 @@ namespace ByteTransfer
             _port = port;
             _requestCallback = Connecting;
 
+            _updateTimer = new Timer(new TimerCallback(Update), null, 10, 10);
+
             Start();
+        }
+
+        private void Update(object state)
+        {
+            if (_stopped) return;
+
+            if (_socket == null) return;
+
+            if (!_socket.Update())
+            {
+                if (_socket.IsOpen())
+                    _socket.CloseSocket();
+            }
         }
 
         private void Connecting(IAsyncResult result)
         {
-            if (Stopped)
+            if (_stopped)
             {
                 _tcpClient.Close();
                 return;
@@ -53,19 +73,19 @@ namespace ByteTransfer
 
         public void Start()
         {
-            if (Started) return;
+            if (_started) return;
 
-            Started = true;
+            _started = true;
 
             _tcpClient.BeginConnect(_host, _port, _requestCallback, null);
         }
 
         public void Stop()
         {
-            if (Stopped) return;
+            if (_stopped) return;
 
-            Stopped = true;
-            Started = false;
+            _stopped = true;
+            _started = false;
 
             if (_socket != null)
                 _socket.DelayedCloseSocket();

48e4d0d2162ac7e35b2a55338ba71015b67f1fb4
ryancheung <ryancheung.go@gmail.com>
2021-02-14 21:48:52 +0800

    Use stackalloc and Span<T> to replace ArrayPool for ARC4 encryption

--- src/Auth/ARC4.cs
+++ src/Auth/ARC4.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Buffers;
 
 namespace ByteTransfer
 {
@@ -25,21 +24,21 @@ namespace ByteTransfer
         public static void Apply(byte[] data, byte[] key, int startIndex = 0, int length = 0)
         {
             //  Key Scheduling Algorithm Phase:
-            //  KSA Phase Step 1: First, the entries of S are set equal to the values of 0 to 255 
+            //  KSA Phase Step 1: First, the entries of S are set equal to the values of 0 to 255
             //                    in ascending order.
-            int[] S = ArrayPool<int>.Shared.Rent(256);
+            Span<int> S = stackalloc int[256];
             for (int _ = 0; _ < 256; _++)
             {
                 S[_] = _;
             }
 
             //  KSA Phase Step 2a: Next, a temporary vector T is created.
-            int[] T = ArrayPool<int>.Shared.Rent(256);
+            Span<byte> T = stackalloc byte[256];
 
-            //  KSA Phase Step 2b: If the length of the key k is 256 bytes, then k is assigned to T.  
+            //  KSA Phase Step 2b: If the length of the key k is 256 bytes, then k is assigned to T.
             if (key.Length == 256)
             {
-                Buffer.BlockCopy(key, 0, T, 0, key.Length);
+                new Span<byte>(key).CopyTo(T);
             }
             else
             {
@@ -57,7 +56,7 @@ namespace ByteTransfer
             int j = 0;
             for (i = 0; i < 256; i++)
             {
-                //  increment j by the sum of S[i] and T[i], however keeping it within the 
+                //  increment j by the sum of S[i] and T[i], however keeping it within the
                 //  range of 0 to 255 using mod (%) division.
                 j = (j + S[i] + T[i]) % 256;
 
@@ -67,18 +66,16 @@ namespace ByteTransfer
                 S[j] = temp;
             }
 
-            ArrayPool<int>.Shared.Return(T);
-
             var dataLength = length > 0 ? Math.Min(length, data.Length) : data.Length;
 
             //  Pseudo random generation algorithm (Stream Generation):
             //  Once the vector S is initialized from above in the Key Scheduling Algorithm Phase,
             //  the input key is no longer used.  In this phase, for the length of the data, we ...
             i = j = 0;
-            byte[] result = ArrayPool<byte>.Shared.Rent(dataLength);
+            Span<byte> result = stackalloc byte[dataLength];
             for (int iteration = 0; iteration < dataLength; iteration++)
             {
-                //  PRGA Phase Step 1. Continously increment i from 0 to 255, starting it back 
+                //  PRGA Phase Step 1. Continously increment i from 0 to 255, starting it back
                 //                     at 0 once we go beyond 255 (this is done with mod (%) division
                 i = (i + 1) % 256;
 
@@ -91,21 +88,17 @@ namespace ByteTransfer
                 S[i] = S[j];
                 S[j] = temp;
 
-                //  PRGA Phase Step 4. Use the result of the sum of S[i] and S[j], mod (%) by 256, 
+                //  PRGA Phase Step 4. Use the result of the sum of S[i] and S[j], mod (%) by 256,
                 //                     to get the index of S that handls the value of the stream value K.
                 int K = S[(S[i] + S[j]) % 256];
 
                 //  PRGA Phase Step 5. Use bitwise exclusive OR (^) with the next byte in the data to
-                //                     produce  the next byte of the resulting ciphertext (when 
+                //                     produce  the next byte of the resulting ciphertext (when
                 //                     encrypting) or plaintext (when decrypting)
                 result[iteration] = Convert.ToByte(data[iteration+startIndex] ^ K);
             }
 
-            ArrayPool<int>.Shared.Return(S);
-
-            Buffer.BlockCopy(result, 0, data, startIndex, dataLength);
-
-            ArrayPool<byte>.Shared.Return(result);
+            result.CopyTo(new Span<byte>(data, startIndex, dataLength));
         }
 
         private byte[] _key;
